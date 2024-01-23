1d62a92f38c89cf3234aa605e382c4c157f12331
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-01-23 18:51:45 +0300

    Добавляет разметку модального окна и реализует её открытие и закрытие

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,28 @@
-/* в этот файл добавляет скрипты*/
+/* Модальное окно */
+
+const modal = document.querySelector('.modal');
+const buttons = Array.from(document.querySelectorAll('.contacts__button'));
+const closeBtn = document.querySelector('.modal__close-button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modal.style.display = 'none';
+  }
+});
